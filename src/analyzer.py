22119da4ede79a6da79de22c5e277325a031db8d
Luv U <zjh98@vip.qq.com>
2020-08-14 23:17:42 +0800

    wip: analysis the datasets

---
+++ src/analyzer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+"""
+@File    :   analyzer.py
+@Desc    :   
+@Project :   src
+@Contact :   1uvu.zhang@gmail.com
+@License :   (C)Copyright 2018-2020, 1UVU.COM
+@WebSite :   1uvu.com
+@Modify Time           @Author        @Version
+------------           -------        --------
+2020/08/14 20:14       1uvu           1.0         
+"""
+import pandas as pd
+import re
+
+""" 手工
+1.
+时间格式化->year mouth：all
+合并
+2.
+publisher，cite，factor 忽略
+3.
+topics 处理 **
+"""
+
+# df = pd.read_excel("./out/all.xlsx")
+# print(df.isnull().any())
+# d = df.dropna(axis=0, subset=['topics'])
+# print(d.isnull().any())
+# d.to_excel("./out/all-no_blank_topics.xlsx", index=False)
+
+df = pd.read_excel("./out/all-no_blank_topics.xlsx")
+topic_items = df["topics"]
+topics = []
+for t_item in topic_items:
+    ts = re.split(",", str(t_item))
+    for t in ts:
+        if t != "":
+            topics.append(t.replace("\xa0", ""))
+from collections import Counter
+
+topics_rank = dict(Counter(topics))
+
+f = open("./out/topics_simple_rank.txt", "w", encoding="utf-8")
+i = 1
+for topic in sorted(topics_rank.items(), key=lambda x: x[1], reverse=True):
+    f.write(f"({i}) {topic[0]}: {topic[1]} \n")
+    print(f"({i}) {topic[0]}: {topic[1]}")
+    i += 1
+f.close()
--- src/utils.py
+++ src/utils.py
@@ -14,25 +14,29 @@
 import pandas as pd
 import os
 
-# 将文件读取出来放一个列表里面
-
-pwd = './out/arv/'  # 获取文件目录
-
-# 新建列表，存放文件名
-file_list = []
-
-# 新建列表存放每个文件数据(依次读取多个相同结构的Excel文件并创建DataFrame)
-dfs = []
-
-for root, dirs, files in os.walk(pwd):  # 第一个为起始路径，第二个为起始路径下的文件夹，第三个是起始路径下的文件。
-    for file in files:
-        file_path = os.path.join(root, file)
-        file_list.append(file_path)  # 使用os.path.join(dirpath, name)得到全路径
-        df = pd.read_excel(file_path)  # 将excel转换成DataFrame
-        dfs.append(df)
-
-# 将多个DataFrame合并为一个
-df = pd.concat(dfs)
-
-# 写入excel文件，不包含索引数据
-df.to_excel('./out/arv/all.xlsx', index=False)
+def merga():
+    # 将文件读取出来放一个列表里面
+    
+    pwd = './out/tf/'  # 获取文件目录
+    
+    # 新建列表，存放文件名
+    file_list = []
+    
+    # 新建列表存放每个文件数据(依次读取多个相同结构的Excel文件并创建DataFrame)
+    dfs = []
+    
+    for root, dirs, files in os.walk(pwd):  # 第一个为起始路径，第二个为起始路径下的文件夹，第三个是起始路径下的文件。
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_list.append(file_path)  # 使用os.path.join(dirpath, name)得到全路径
+            df = pd.read_excel(file_path)  # 将excel转换成DataFrame
+            dfs.append(df)
+    
+    # 将多个DataFrame合并为一个
+    df = pd.concat(dfs)
+    
+    # 写入excel文件，不包含索引数据
+    df.to_excel('./out/tf/all.xlsx', index=False)
+    
+if __name__ == '__main__':
+    merga()
