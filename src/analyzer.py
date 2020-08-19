995fe8637f9cc61a7b9694a07d67f55b5ab8df17
Luv U <zjh98@vip.qq.com>
2020-08-19 23:26:37 +0800

    filter dataset by title

--- src/analyzer.py
+++ src/analyzer.py
@@ -12,6 +12,7 @@
 2020/08/14 20:14       1uvu           1.0         
 """
 import pandas as pd
+import numpy as np
 import re
 
 """ 手工
@@ -40,14 +41,14 @@ topics 处理 **
 # utils
 
 # remove blank
-df = pd.read_excel("./out/all.xlsx")
+df = pd.read_excel("./out/all-filtered.xlsx")
 print(df.isnull().any())
-d = df.dropna(axis=0, subset=["topics", "date", "title"])
+d = df.dropna(axis=0, subset=["topics"])
 print(d.isnull().any())
-d.to_excel("./out/all-topics.xlsx", index=False)
+d.to_excel("./out/all-filtered-has_topics.xlsx", index=False)
 
 # rank
-df = pd.read_excel("./out/all-topics.xlsx")
+df = pd.read_excel("./out/all-filtered-has_topics.xlsx")
 topic_items = df["topics"]
 topics = []
 for t_item in topic_items:
@@ -66,3 +67,34 @@ for topic in sorted(topics_rank.items(), key=lambda x: x[1], reverse=True):
     print(f"({i}) {topic[0]}: {topic[1]}")
     i += 1
 f.close()
+
+# # rm titles
+#
+# """
+# B/blockchain-B/based
+# B/blockchain B/based
+# B/based on
+# """
+# ts = []
+# with open("./out/titles-6.txt", "r", encoding="utf-8") as f:
+#     for t in f.readlines():
+#         if "based" not in t:
+#             ts.append(t)
+#
+# f.close()
+# with open("./out/titles-7.txt", "w", encoding="utf-8") as f:
+#     for t in ts:
+#         f.write(t)
+#
+# df = pd.read_excel("./out/all-filtered.xlsx")
+# f = open("./out/titles.txt", "r", encoding="utf-8")
+# titles = [s.strip() for s in f.readlines()]
+#
+# # i=0
+# # for t in df["title"]:
+# #     print(t + ": " + titles[i])
+# #     print(t in titles)
+# #     i+=1
+# df1 = df[df['title'].isin(titles)]
+#
+# df1.to_excel("./out/all-filter_by_title.xlsx", index=False)
