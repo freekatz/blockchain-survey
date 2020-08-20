a1454f23a18caff62477f591bbbbfca71be3532b
ZJH <zjh98@vip.qq.com>
2020-08-20 20:17:02 +0800

    filter dataset by manual

--- src/analyzer.py
+++ src/analyzer.py
@@ -40,53 +40,54 @@ topics 处理 **
 # merge
 # utils
 
-# remove blank
-df = pd.read_excel("./out/all-filtered.xlsx")
-print(df.isnull().any())
-d = df.dropna(axis=0, subset=["topics"])
-print(d.isnull().any())
-d.to_excel("./out/all-filtered-has_topics.xlsx", index=False)
+# # remove blank
+# df = pd.read_excel("./out/all-filtered.xlsx")
+# print(df.isnull().any())
+# d = df.dropna(axis=0, subset=["topics"])
+# print(d.isnull().any())
+# d.to_excel("./out/all-filtered-has_topics.xlsx", index=False)
 
-# rank
-df = pd.read_excel("./out/all-filtered-has_topics.xlsx")
-topic_items = df["topics"]
-topics = []
-for t_item in topic_items:
-    ts = re.split(",", str(t_item))
-    for t in ts:
-        if t != "":
-            topics.append(t.replace("\xa0", ""))
-from collections import Counter
-
-topics_rank = dict(Counter(topics))
-
-f = open("./out/topics_simple_rank.txt", "w", encoding="utf-8")
-i = 1
-for topic in sorted(topics_rank.items(), key=lambda x: x[1], reverse=True):
-    f.write(f"({i}) {topic[0]}: {topic[1]} \n")
-    print(f"({i}) {topic[0]}: {topic[1]}")
-    i += 1
-f.close()
-
-# # rm titles
+# # rank
+# df = pd.read_excel("./out/all-filtered-has_topics.xlsx")
+# topic_items = df["topics"]
+# topics = []
+# for t_item in topic_items:
+#     ts = re.split(",", str(t_item))
+#     for t in ts:
+#         if t != "":
+#             topics.append(t.replace("\xa0", ""))
+# from collections import Counter
 #
-# """
-# B/blockchain-B/based
-# B/blockchain B/based
-# B/based on
-# """
+# topics_rank = dict(Counter(topics))
+#
+# f = open("./out/topics_simple_rank.txt", "w", encoding="utf-8")
+# i = 1
+# for topic in sorted(topics_rank.items(), key=lambda x: x[1], reverse=True):
+#     f.write(f"({i}) {topic[0]}: {topic[1]} \n")
+#     print(f"({i}) {topic[0]}: {topic[1]}")
+#     i += 1
+# f.close()
+
+# # # rm titles
+# #
+# # """
+# # B/based
+# # P/powered
+# # B/blockchain-
+# # E/enabled
+# # """
 # ts = []
-# with open("./out/titles-6.txt", "r", encoding="utf-8") as f:
+# with open("./out/titles.txt", "r", encoding="utf-8") as f:
 #     for t in f.readlines():
-#         if "based" not in t:
+#         if "Enabled" not in t:
 #             ts.append(t)
 #
 # f.close()
-# with open("./out/titles-7.txt", "w", encoding="utf-8") as f:
+# with open("./out/titles.txt", "w", encoding="utf-8") as f:
 #     for t in ts:
 #         f.write(t)
-#
-# df = pd.read_excel("./out/all-filtered.xlsx")
+
+# df = pd.read_excel("./out/all-test.xlsx")
 # f = open("./out/titles.txt", "r", encoding="utf-8")
 # titles = [s.strip() for s in f.readlines()]
 #
@@ -97,4 +98,30 @@ f.close()
 # #     i+=1
 # df1 = df[df['title'].isin(titles)]
 #
-# df1.to_excel("./out/all-filter_by_title.xlsx", index=False)
+# df1.to_excel("./out/all-filtered.xlsx", index=False)
+
+df = pd.read_excel("./out/all-filtered.xlsx")
+
+df1 = pd.DataFrame(columns=["title", "abstract", "url"])
+df1["title"] = df["title"]
+df1["abstract"] = df["abstract"]
+df1["url"] = df["url"]
+print(df1)
+
+df1.to_html("./out/tmp.htm", encoding="utf-8")
+
+# # 处理 index.txt 时加 1
+# df = pd.read_excel("./out/all-filtered-2.xlsx")
+# with open("./out/index.txt", "r", encoding="utf-8") as f:
+#     indexs = [int(i.strip()) for i in f.readlines()]
+#
+#     titles = []
+#     for i in range(len(df["title"])):
+#         if i not in indexs:
+#             titles.append(df["title"][i])
+#
+#     df1 = df[df["title"].isin(titles)]
+#
+#     print(df1)
+#
+#     df1.to_excel("./out/all-filtered-3.xlsx", encoding="utf-8", index=False)
