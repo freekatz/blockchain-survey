112cbd89b1928fec76adc1932c74cc3fb2a96ae1
ZJH <zjh98@vip.qq.com>
2020-10-13 19:37:44 +0800

    add security topics

--- src/tests.py
+++ src/tests.py
@@ -11,7 +11,7 @@
 ------------           -------        --------
 2020/09/01 12:08       1uvu           1.0         
 """
-import nltk.stem as ns
+import nltk.stem as nots
 
 # from utils import *
 #
@@ -70,89 +70,104 @@ import nltk.stem as ns
 # if __name__ == '__main__':
 #     app.run_server()
 
+#
+# import pandas as pd
+# import numpy as np
+# from matplotlib import pyplot as plt
+#
+# # Import Data
+# df = pd.read_excel('out/rank/all-ranking-freq.xlsx', index_col=0)
+#
+# limit = 30
+# ddf = df.sort_values("all")[0 - limit:]
+# ddf = ddf.drop(index=np.nan)
+# # Prepare Data
+# x = ddf.index.tolist()
+# # x = [i for i in range(30)]
+#
+# vstack = []
+# columns = ddf.columns[1:]
+# for t in columns:
+#     yi = ddf[t].values.tolist()
+#     print(yi)
+#     vstack.append(yi)
+#
+# mycolors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:brown', 'tab:grey', 'tab:pink', 'tab:olive']
+# y = np.vstack(vstack)
+# # Plot for each column
+# labs = columns
+# ax = plt.gca()
+# ax.stackplot(x, y, labels=labs, colors=mycolors, alpha=0.8)
+#
+# # Decorations
+# ax.set_title('f1', fontsize=18)
+# ax.set(ylim=[0, 360])
+# ax.legend(fontsize=10, ncol=4)
+# plt.xticks(x[::1], fontsize=6, horizontalalignment='left', rotation=320)
+# plt.yticks(np.arange(0, 360, 50), fontsize=8)
+# plt.xlim(x[0], x[-1])
+#
+# # Lighten borders
+# plt.gca().spines["top"].set_alpha(0)
+# plt.gca().spines["bottom"].set_alpha(.3)
+# plt.gca().spines["right"].set_alpha(0)
+# plt.gca().spines["left"].set_alpha(.3)
+#
+# plt.rcParams['figure.dpi'] = 600
+# plt.rcParams['savefig.dpi'] = 600
+# plt.grid(axis="x", linestyle=":", linewidth=0.5)
+# plt.tight_layout()
+# plt.savefig("./out/rank/f1.png")
+#
+# '''-------------------'''
+# mycolors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:brown', 'tab:grey', 'tab:pink', 'tab:olive']
+# columns = ["all", "2016", "2017", "2018", "2019", "2020"]
+#
+# # Draw Plot
+# # fig, ax = plt.subplots(1, 1, figsize=(16, 9), dpi=300)
+# ax=plt.gca()
+# i = 0
+# for t in columns:
+#     yi = ddf[t].values.tolist()
+#     ax.fill_between(x, y1=yi, y2=yi, label=columns[i], alpha=0.8, color=mycolors[i], linewidth=1)
+#     i += 1
+#
+# # Decorations
+# ax.set_title('f2', fontsize=18)
+# ax.legend(loc='best', fontsize=12, ncol=4)
+# plt.xticks(x[::1], fontsize=6, horizontalalignment='left', rotation=320)
+# plt.yticks(np.arange(0, 360.0, 50), fontsize=8)
+# plt.xlim(x[0], x[-1])
+#
+# # # Draw Tick lines
+# # for y in np.arange(0, 150.0, 30):
+# #     plt.hlines(y, xmin=0, xmax=len(x), colors='black', alpha=0.3, linestyles="--", lw=0.5)
+#
+# # Lighten borders
+# plt.gca().spines["top"].set_alpha(0)
+# plt.gca().spines["bottom"].set_alpha(.3)
+# plt.gca().spines["right"].set_alpha(0)
+# plt.gca().spines["left"].set_alpha(.3)
+#
+# plt.rcParams['figure.dpi'] = 600
+# plt.rcParams['savefig.dpi'] = 600
+# plt.tight_layout()
+# plt.grid(axis="x", linestyle=":", linewidth=0.5)
+# plt.savefig("./out/rank/f2.png")
 
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
 
-# Import Data
-df = pd.read_excel('out/rank/all-ranking-freq.xlsx', index_col=0)
+import re
 
-limit = 30
-ddf = df.sort_values("all")[0 - limit:]
-ddf = ddf.drop(index=np.nan)
-# Prepare Data
-x = ddf.index.tolist()
-# x = [i for i in range(30)]
+from utils import *
 
-vstack = []
-columns = ddf.columns[1:]
-for t in columns:
-    yi = ddf[t].values.tolist()
-    print(yi)
-    vstack.append(yi)
-
-mycolors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:brown', 'tab:grey', 'tab:pink', 'tab:olive']
-y = np.vstack(vstack)
-# Plot for each column
-labs = columns
-ax = plt.gca()
-ax.stackplot(x, y, labels=labs, colors=mycolors, alpha=0.8)
-
-# Decorations
-ax.set_title('f1', fontsize=18)
-ax.set(ylim=[0, 360])
-ax.legend(fontsize=10, ncol=4)
-plt.xticks(x[::1], fontsize=6, horizontalalignment='left', rotation=320)
-plt.yticks(np.arange(0, 360, 50), fontsize=8)
-plt.xlim(x[0], x[-1])
-
-# Lighten borders
-plt.gca().spines["top"].set_alpha(0)
-plt.gca().spines["bottom"].set_alpha(.3)
-plt.gca().spines["right"].set_alpha(0)
-plt.gca().spines["left"].set_alpha(.3)
-
-plt.rcParams['figure.dpi'] = 600
-plt.rcParams['savefig.dpi'] = 600
-plt.grid(axis="x", linestyle=":", linewidth=0.5)
-plt.tight_layout()
-plt.savefig("./out/rank/f1.png")
-
-'''-------------------'''
-mycolors = ['tab:red', 'tab:blue', 'tab:green', 'tab:orange', 'tab:brown', 'tab:grey', 'tab:pink', 'tab:olive']
-columns = ["all", "2016", "2017", "2018", "2019", "2020"]
-
-# Draw Plot
-# fig, ax = plt.subplots(1, 1, figsize=(16, 9), dpi=300)
-ax=plt.gca()
-i = 0
-for t in columns:
-    yi = ddf[t].values.tolist()
-    ax.fill_between(x, y1=yi, y2=yi, label=columns[i], alpha=0.8, color=mycolors[i], linewidth=1)
-    i += 1
-
-# Decorations
-ax.set_title('f2', fontsize=18)
-ax.legend(loc='best', fontsize=12, ncol=4)
-plt.xticks(x[::1], fontsize=6, horizontalalignment='left', rotation=320)
-plt.yticks(np.arange(0, 360.0, 50), fontsize=8)
-plt.xlim(x[0], x[-1])
-
-# # Draw Tick lines
-# for y in np.arange(0, 150.0, 30):
-#     plt.hlines(y, xmin=0, xmax=len(x), colors='black', alpha=0.3, linestyles="--", lw=0.5)
-
-# Lighten borders
-plt.gca().spines["top"].set_alpha(0)
-plt.gca().spines["bottom"].set_alpha(.3)
-plt.gca().spines["right"].set_alpha(0)
-plt.gca().spines["left"].set_alpha(.3)
-
-plt.rcParams['figure.dpi'] = 600
-plt.rcParams['savefig.dpi'] = 600
-plt.tight_layout()
-plt.grid(axis="x", linestyle=":", linewidth=0.5)
-plt.savefig("./out/rank/f2.png")
-
-
+a = open("./out/rank/freq/txt/all_rank.txt", "r")
+topics = [re.search(r"\) (.*): ", l).groups()[0] for l in a.readlines()]
+s = open("./res/security.txt", "r")
+securities = [l.replace("\n", "") for l in s.readlines()]
+a.close()
+s.close()
+print(topics)
+not_securities = [l for l in topics if l not in securities]
+ns = open("./res/not-security.txt", "w")
+ns.write("\n".join(not_securities))
+ns.close()
